26ca5de8d348dc785f10acc4f630e3b3e47b6c69
Milan Pratap Sengar <milanpratap1407@gmail.com>
2025-04-13 01:23:31 +0530

    changes

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -31,8 +31,8 @@ function LoginForm(){
     }
 
     return (
-        <div className=" backdrop-blur-md bg-white/30 border border-white/20 shadow-md text-white absolute top-[18%] left-[35%] w-[30%] h-[65%] border rounded-md flex items-center justify-center">
-            <div className= "w-[85%] h-[90%] flex-col">
+        <div className="flex items-center justify-center backdrop-blur-md bg-white/30 border border-white/20 shadow-md border rounded-md text-white absolute top-[15%] left-[10%] w-[80%] h-[70%]  sm:w-[70%] sm:h-[70%] sm:top-[12%] sm:left-[15%] md:top-[18%] md:left-[26%] md:w-[50%] md:h-[65%] lg:w-[30%] lg:left-[35%]">
+            <div className= "flex-col w-[95%] h-[90%] ">
 
                 <div className="flex items-center justify-center mb-8">
                     <h1 className="text-[35px] font-semibold">Login</h1>
@@ -40,23 +40,23 @@ function LoginForm(){
 
                 <form onSubmit={submitHandler} className="h-[60%] w-[100%]">
                     <div className="relative  m-5">
-                        <input type="email" placeholder="Email Address" className="border border-2 border-white w-[100%] p-3 mb-4 rounded-[100px]" name="username" value={username} onChange={changeHandler}/>
-                        <FaUser className="absolute top-4 right-5"/>
+                        <input type="email" placeholder="Email Address" className="border  text-sm md:text-[15px] border-2 border-white w-[100%] text-overflow text-[12px] p-3 mb-4 rounded-[100px]" name="username" value={username} onChange={changeHandler}/>
+                        <FaUser className="absolute top-[25%] right-3"/>
                     </div>
                     <div className="relative  m-5">
-                        <input type="password" placeholder="Password" className="border border-2 border-white w-[100%] p-3 rounded-[100px]" name="password" value={password} onChange={changeHandler}/>
-                        <RiLockPasswordFill className="absolute top-4 right-5" />
+                        <input type="password" placeholder="Password" className="border border-2 text-sm md:text-[15px] border-white w-[100%] p-3 rounded-[100px]" name="password" value={password} onChange={changeHandler}/>
+                        <RiLockPasswordFill className="absolute top-4 right-3" />
                     </div>
                     <NavLink>
-                        <p className="ml-[63%] text-sm  w-[33%] mt-[-10px]">Forgot Password?</p>
+                        <p className="ml-[40%] text-[12px] text-right w-[50%] mt-[-10px]">Forgot Password?</p>
                     </NavLink>
                     <div className=" m-5">
-                        <button type="submit" className="bg-white text-black cursor-pointer text-lg font-semibold w-[100%] p-3 mt-2 rounded-[100px]">Login</button>
+                        <button type="submit" className="bg-white text-black cursor-pointer active:scale-90  transition-transform duration-200 text-lg font-semibold w-[100%] p-3 mt-2 rounded-[100px]">Login</button>
                     </div>
                 </form>
-                <div className=" mt-5 ml-20 font-sm">
-                    <p>Don't have an account? {"  "}
-                        <NavLink className="font-bold" to={"/signup"}>
+                <div className=" text-[15px] text-center w-[85%] mt-5 md:mt-8 lg:mt-7  ml-7 font-sm">
+                    <p>Don't have an account? {"  "} <br />
+                        <NavLink className="font-bold underline text-[#0000CD] hover:text-[#005A9C]" to={"/signup"}>
                             Register
                         </NavLink>
                     </p>
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -18,6 +18,7 @@ function Signup(){
             toast.error("Password Mismatched.")
             return;
         }
+        console.log(formData);
         navigate("/weather")
     }
 
@@ -31,35 +32,35 @@ function Signup(){
     }
 
     return (
-        <div className=" backdrop-blur-md bg-white/30 border border-white/20 shadow-md text-white absolute top-[10%] left-[32%] w-[35%] h-[80%] border rounded-md flex items-center justify-center">
-            <div className="w-[85%] h-[90%] flex-col">
+        <div className=" backdrop-blur-md bg-white/30 border border-white/20 shadow-md text-white absolute top-[10%] left-[10%] w-[80%] h-[80%] sm:w-[70%] sm:h-[80%] sm:top-[12%] sm:left-[17%] md:top-[13%] md:left-[28%] md:w-[50%] md:h-[75%] lg:left-[10%] lg:w-[40%] lg:h-[75%] lg:left-[30%] border rounded-md flex items-center justify-center">
+            <div className="w-[85%] h-[90%] md:w-[90%] md:h-[95%] flex-col">
 
                 <div className="flex items-center justify-center mb-8">
                     <h1 className="text-[35px] font-semibold">Signup</h1>
                 </div>
 
-                <form onSubmit={submitHandler} className="h-[60%] w-[100%]">
+                <form onSubmit={submitHandler} className="h-[60%] w-[100%] lg:h-[70%] ">
                     <div className="flex justify-between m-5">
-                        <input type="text"  placeholder="FirstName" className="border border-2 border-white w-[45%] p-3 rounded-[100px]" name="firstName" value={firstName} onChange={changeHandler}/>
-                        <input type="text"  placeholder="LastName" className="border border-2 border-white w-[45%] p-3 rounded-[100px]" name="lastName" value={lastName} onChange={changeHandler}/>
+                        <input type="text"  placeholder="FirstName" className="border h-[5%] lg:p-4 text-sm md:text-[15px]  border-2 border-white w-[45%] p-3 rounded-[100px]" name="firstName" value={firstName} onChange={changeHandler}/>
+                        <input type="text"  placeholder="LastName" className="border h-[5%] lg:p-4  text-sm md:text-[15px] border-2 border-white w-[45%] p-3 rounded-[100px]" name="lastName" value={lastName} onChange={changeHandler}/>
                     </div>
                     <div className="m-5">
-                        <input type="email"  placeholder="Email" className="border border-2 border-white w-[100%] p-3 rounded-[100px]" name="email" value={email} onChange={changeHandler}/>
+                        <input type="email"  placeholder="Email" className="border h-[5%] lg:p-4  text-sm md:text-[15px] border-2 border-white w-[100%] p-3 rounded-[100px]" name="email" value={email} onChange={changeHandler}/>
                     </div>
                     <div className="m-5">
-                        <input type="number"  placeholder="Contact No" className="border border-2 border-white w-[100%] p-3 rounded-[100px]" name="contact" value={contact} onChange={changeHandler}/>
+                        <input type="number"  placeholder="Contact No" className="border text-sm md:text-[15px] lg:p-4  h-[5%] border-2 border-white w-[100%] p-3 rounded-[100px]" name="contact" value={contact} onChange={changeHandler}/>
                     </div>
                     <div className="flex justify-between m-5">
-                        <input type="password"  placeholder="Password" className="border border-2 border-white w-[45%] p-3 rounded-[100px]" name="password" value={password} onChange={changeHandler}/>
-                        <input type="password"  placeholder="Confirm Password" className="border border-2 border-white w-[45%] p-3 rounded-[100px]" name="confirmPassword" value={confirmPassword} onChange={changeHandler}/>
+                        <input type="password"  placeholder="Password" className="border h-[5%] text-sm md:text-[15px] lg:p-4  border-2 border-white w-[45%] p-3 rounded-[100px]" name="password" value={password} onChange={changeHandler}/>
+                        <input type="password"  placeholder="Confirm Password" className="border h-[5%] text-sm md:text-[15px] lg:p-4  border-2 border-white w-[45%] p-3 rounded-[100px]" name="confirmPassword" value={confirmPassword} onChange={changeHandler}/>
                     </div>
                     <div className=" m-5">
-                        <button type="submit" className="bg-white text-black cursor-pointer text-lg font-semibold w-[100%] p-3 mt-5 rounded-[100px]">Create Account</button>
+                        <button type="submit" className="bg-white text-black cursor-pointer active:scale-90  lg:p-3 transition-transform duration-200 h-[5%] text-md font-semibold w-[100%] p-2 mt-5 rounded-[100px]">Create Account</button>
                     </div>
                 </form>
-                <div className="mt-[15%] ml-[22%] font-sm">
-                    <p>Already have a account? {"  "}
-                        <NavLink className="font-bold" to={"/"}>
+                <div className="mt-[15%] text-sm md:text-[15px] lg:mt-[2%]  text-center ml-[5%] font-sm">
+                    <p>Already have a account? {"  "} <br />
+                        <NavLink className="font-bold underline text-[#0000CD] hover:text-[#005A9C] " to={"/"}>
                             Login
                         </NavLink>
                     </p>
