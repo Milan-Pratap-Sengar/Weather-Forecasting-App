76c099279b898d104eebb62dfa244fc75eaf3b46
Milan Pratap Sengar <milanpratap1407@gmail.com>
2025-04-13 15:10:41 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Weather-Forecasting-App/"
+  // base:"/Weather-Forecasting-App/"
 })
